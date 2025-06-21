45e288d470fae5651c8479f7e076ca17e8b1659a
overbugger <okorosamuelchika@gmail.com>
2025-06-21 10:58:00 +0200

    implement validate jwt token

--- auth-service/src/main/java/com/endpoint/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/endpoint/authservice/util/JwtUtil.java
@@ -1,13 +1,16 @@
 package com.endpoint.authservice.util;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
 
@@ -33,6 +36,17 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) //10hrs
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+
+        } catch (JwtException e) {
+            throw new JwtException("Invalid JWT");
+        }
     }
 
 }
